c33bbe4a53f72498688fa73e5990480923191774
Pravin Kumar Mathuri <pravinkumarmathuri@gmail.com>
2022-09-04 17:35:28 +0530

    logical_operator_javacript

---
+++ problem4.js
@@ -0,0 +1,9 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a = 10;
+let b = 15;
+let c = 20;
+
+if (a<b && b<c || c>b && c>a) {
+  console.log ("c is greatest");
+} 
